2efe6f05b1de6630084673d8467f8db8c0bb6168
pramodbn <begurnagaraj.p@northeastern.edu>
2023-10-18 20:18:01 -0400

    updates

--- routes/assignmentRoutes.js
+++ routes/assignmentRoutes.js
@@ -4,12 +4,18 @@ var helper = require('../utils/helper');
 var assignment = require('../controllers/assignment');
 
 router.post('/', helper.aAuthCheck, assignment.createNewAssignment);
-
+router.post('/*', helper.invalidPath);
 router.get('/:id', helper.aAuthCheck, assignment.getAssignment);
-
+//router.get('/*', helper.invalidPath);
 router.get('/', helper.aAuthCheck, assignment.getAllAssignments);
-router.put('/:id', helper.aAuthCheck, assignment.putAssignmentInfo);
-
+router.put(
+  '/:id',
+  helper.aAuthCheck,
+  helper.checkQueryParams,
+  assignment.putAssignmentInfo
+);
+router.put('/*', helper.invalidPath);
 router.delete('/:id', helper.aAuthCheck, assignment.deleteAssignmentInfo);
+router.delete('/*', helper.invalidPath);
 
 module.exports = router;
