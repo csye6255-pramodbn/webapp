c7233191c359eec6114ad20ecf8a80089c92a04f
pramodbn <begurnagaraj.p@northeastern.edu>
2023-12-04 20:43:19 -0500

    v1

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (!res) {
 }
 
 //API FOR ASSIGNMENTS
-app.use('/v2/assignments', assignment);
+app.use('/v1/assignments', assignment);
 //API ENDPOINTS FOR healthz
 app.use('/healthz', healthzCheck);
 app.patch('/*', (req, res) => { 
